c3fb3ae5c75317df1e726f5375ae13a70c16f217
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-01 12:45:21 +0300

    add 8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
+
+const onFormSubmit = (event) => {
+  const element = event.currentTarget.elements;
+
+  event.preventDefault();
+
+  if (!element.email.value || !element.password.value) {
+    alert('Будь ласка заповніть усі поля!');
+  } else {
+    alert('Дані завантажені!');
+
+    const formData = new FormData(event.currentTarget);
+    formData.forEach ( (value, name) => {
+      console.log(`${name}: ${value}`);
+    });
+
+    event.currentTarget.reset();
+  }
+};
+
+const onEscClick = (event) => {
+  if (event.code === 'Escape') {
+    document.querySelector('.login-form [name="email"]').value = "";
+    document.querySelector('.login-form [name="password"]').value = "";
+  }
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+window.addEventListener('keyup', onEscClick);
+
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  btnChangeColor: document.querySelector('button.change-color'),
+  textColor: document.querySelector('span.color'),
+};
+
+const onBtnClick = (event) => {
+  const randomColor = getRandomHexColor();
+  
+  document.body.style.backgroundColor = `${randomColor}`;
+  refs.textColor.textContent = `${randomColor}`;
+};
+
+refs.btnChangeColor.addEventListener('click', onBtnClick);
